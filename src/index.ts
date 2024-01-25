5863aaa04ad5e9d4cb6224b8bfa26b4b268d609c
Brandon Hoffey <brandon.hoffey@yahoo.com>
2024-01-24 19:05:24 -0700

    added portfolio.html

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log("Hello World");
+document.addEventListener("DOMContentLoaded", function () {
+  const portfolioButton = document.getElementById("portfolioButton");
+
+  if (portfolioButton) {
+    portfolioButton.addEventListener("click", function () {
+      window.location.href = "http://127.0.0.1:5500/ellieportfolio/src/portfolio/portfolio.html";
+    });
+  }
+});
