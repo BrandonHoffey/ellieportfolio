45c3b30e3d5e94f598ac80333fb65a5655bcafdb
Brandon Hoffey <brandon.hoffey@yahoo.com>
2024-01-29 08:41:04 -0700

    Created About and Contact pages

---
+++ src/contact/Contact.ts
@@ -0,0 +1,1 @@
+console.log("Hello from Contact.ts")
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,30 @@ document.addEventListener("DOMContentLoaded", function () {
 
   if (portfolioButton) {
     portfolioButton.addEventListener("click", function () {
-      window.location.href = "http://127.0.0.1:5500/ellieportfolio/src/portfolio/portfolio.html";
+      window.location.href =
+        "http://127.0.0.1:5500/ellieportfolio/src/portfolio/portfolio.html";
+    });
+  }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const aboutButton = document.getElementById("aboutButton");
+
+  if (aboutButton) {
+    aboutButton.addEventListener("click", function () {
+      window.location.href =
+        "http://127.0.0.1:5500/ellieportfolio/src/about/About.html";
+    });
+  }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const contactButton = document.getElementById("contactButton");
+
+  if (contactButton) {
+    contactButton.addEventListener("click", function () {
+      window.location.href =
+        "http://127.0.0.1:5500/ellieportfolio/src/contact/Contact.html";
     });
   }
 });
