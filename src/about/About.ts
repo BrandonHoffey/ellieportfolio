ae957518d28eb7da3ceedeb35a90cfb4912e5cec
BrandonHoffey <brandon.hoffey@yahoo.com>
2024-03-15 17:04:22 -0600

    About me basically done

--- src/about/About.ts
+++ src/about/About.ts
@@ -9,19 +9,19 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 });
 
-document.addEventListener("DOMContentLoaded", function () {
-  const images = document.querySelectorAll(".aboutImage");
-  images.forEach((image) => {
-    const imageElement = image as HTMLImageElement;
-    imageElement.addEventListener("mouseenter", function () {
-      imageElement.style.transform = "scale(2)";
-      imageElement.style.transition = "transform 0.3s";
-      imageElement.style.zIndex = "1";
-    });
-    imageElement.addEventListener("mouseleave", function () {
-      imageElement.style.transform = "scale(1)";
-      imageElement.style.zIndex = "0";
-      imageElement.style.transition = "transform 0.3s";
-    });
-  });
-});
+// document.addEventListener("DOMContentLoaded", function () {
+//   const images = document.querySelectorAll(".aboutImage");
+//   images.forEach((image) => {
+//     const imageElement = image as HTMLImageElement;
+//     imageElement.addEventListener("mouseenter", function () {
+//       imageElement.style.transform = "scale(2)";
+//       imageElement.style.transition = "transform 0.3s";
+//       imageElement.style.zIndex = "1";
+//     });
+//     imageElement.addEventListener("mouseleave", function () {
+//       imageElement.style.transform = "scale(1)";
+//       imageElement.style.zIndex = "0";
+//       imageElement.style.transition = "transform 0.3s";
+//     });
+//   });
+// });
