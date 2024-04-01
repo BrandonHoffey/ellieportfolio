e6988e9b342963c27a2f958a8537f6a8f4feef17
BrandonHoffey <brandon.hoffey@yahoo.com>
2024-03-31 22:32:27 -0600

    added enlarged image container

--- dist/photography/Photography.js
+++ dist/photography/Photography.js
@@ -7,4 +7,27 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 });
+document.addEventListener("DOMContentLoaded", function () {
+    const photos = document.querySelectorAll(".photographyPhotos img");
+    photos.forEach(photo => {
+        photo.addEventListener("click", function (event) {
+            const clickedImageSrc = event.target.src;
+            const enlargedImage = document.getElementById("enlargedImage");
+            enlargedImage.src = clickedImageSrc;
+            const enlargedImageContainer = document.getElementById("enlargedImageContainer");
+            if (enlargedImageContainer) {
+                enlargedImageContainer.style.display = "block";
+                document.body.style.overflow = "hidden";
+            }
+        });
+    });
+    const closeButton = document.getElementById("closeButton");
+    const enlargedImageContainer = document.getElementById("enlargedImageContainer");
+    closeButton === null || closeButton === void 0 ? void 0 : closeButton.addEventListener("click", function () {
+        if (enlargedImageContainer) {
+            enlargedImageContainer.style.display = "none";
+            document.body.style.overflow = "";
+        }
+    });
+});
 //# sourceMappingURL=Photography.js.map
--- src/photography/Photography.ts
+++ src/photography/Photography.ts
@@ -7,3 +7,30 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const photos = document.querySelectorAll(".photographyPhotos img");
+
+  photos.forEach(photo => {
+    photo.addEventListener("click", function (event) {
+      const clickedImageSrc = (event.target as HTMLImageElement).src;
+      const enlargedImage = document.getElementById("enlargedImage") as HTMLImageElement;
+      enlargedImage.src = clickedImageSrc;
+      const enlargedImageContainer = document.getElementById("enlargedImageContainer");
+      if (enlargedImageContainer) {
+        enlargedImageContainer.style.display = "block";
+        document.body.style.overflow = "hidden";
+      }
+    });
+  });
+
+  const closeButton = document.getElementById("closeButton");
+  const enlargedImageContainer = document.getElementById("enlargedImageContainer");
+
+  closeButton?.addEventListener("click", function () {
+    if (enlargedImageContainer) {
+      enlargedImageContainer.style.display = "none";
+      document.body.style.overflow = "";
+    }
+  });
+});
