7c159c6830e1edea63d60037083d180adcef8836
BrandonHoffey <brandon.hoffey@yahoo.com>
2024-05-20 14:39:57 -0600

    moved index files into root folder

---
+++ index.ts
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const photographyButton = document.getElementById("photographyButton");
+
+  if (photographyButton) {
+    photographyButton.addEventListener("click", function () {
+      window.location.href =
+        "http://127.0.0.1:5500/src/photography/Photography.html";
+    });
+  }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const aboutButton = document.getElementById("aboutButton");
+
+  if (aboutButton) {
+    aboutButton.addEventListener("click", function () {
+      window.location.href =
+        "http://127.0.0.1:5500/src/about/About.html";
+    });
+  }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const cinematographyButton = document.getElementById("cinematographyButton");
+
+  if (cinematographyButton) {
+    cinematographyButton.addEventListener("click", function () {
+      window.location.href =
+        "http://127.0.0.1:5500/src/cinematography/Cinematography.html";
+    });
+  }
+});
